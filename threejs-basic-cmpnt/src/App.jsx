4ed3f6ec41443626d13af158cf25dbabade8b311
isai7710 <isai10sanchez@gmail.com>
2024-09-18 15:03:52 -0600

    added buttons along with state variables to toggle the color of the objects in scene as well as the background div container color

--- threejs-basic-cmpnt/src/App.jsx
+++ threejs-basic-cmpnt/src/App.jsx
@@ -1,12 +1,26 @@
 import "./App.css";
+import { useState } from "react";
 import ThreeJSScene from "./components/three-js-scene";
 
 function App() {
+  const [color, setColor] = useState(true);
+  const [bgColor, setBGColor] = useState(true);
+
   return (
     <>
       <div className="flex flex-col gap-2">
-        <ThreeJSScene />
+        <ThreeJSScene color={color} bgColor={bgColor} />
         <p>First Three.js Scene</p>
+        <div className="flex justify-between gap-2">
+          <button onClick={() => setColor(!color)} className="w-1/2">
+            Click to change object color to{" "}
+            <span>{color ? `blue` : `red`}</span>
+          </button>
+          <button onClick={() => setBGColor(!bgColor)} className="w-1/2">
+            Click to change background color to{" "}
+            <span>{bgColor ? `dirty green` : `normal`}</span>
+          </button>
+        </div>
       </div>
     </>
   );
