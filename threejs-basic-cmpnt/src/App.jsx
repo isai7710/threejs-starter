5e5db6f843b16d2bb5b0d32861fae7ebf03cd9e8
isai7710 <isai10sanchez@gmail.com>
2024-09-18 14:00:59 -0600

    used new threejs component name

--- threejs-basic-cmpnt/src/App.jsx
+++ threejs-basic-cmpnt/src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
-import ThreeJSCube from "./components/three-js-cube";
+import ThreeJSScene from "./components/three-js-scene";
 
 function App() {
   return (
     <>
       <div className="flex flex-col gap-2">
-        <ThreeJSCube />
+        <ThreeJSScene />
         <p>First Three.js Scene</p>
       </div>
     </>
