a896dab0938ea0a7f7b8ed89d1ca51e378f3aef6
isai7710 <isai10sanchez@gmail.com>
2024-09-19 15:09:33 -0600

    decreased canvas size

--- r3f-drei/src/App.jsx
+++ r3f-drei/src/App.jsx
@@ -1,5 +1,5 @@
-import { OrbitControls } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Sparkles } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 
 const RotatingCube = () => {
@@ -15,7 +15,15 @@ const RotatingCube = () => {
   return (
     <mesh ref={meshRef}>
       <cylinderGeometry args={[1, 1, 1]} />
-      <meshLambertMaterial color="#468585" emissive="#468585" />
+      <meshLambertMaterial color="#692156" emissive="#692156" />
+      <Sparkles
+        count={100}
+        scale={1}
+        size={6}
+        speed={0.002}
+        noise={0.2}
+        color="green"
+      />
     </mesh>
   );
 };
@@ -24,16 +32,16 @@ const App = () => {
   return (
     <Canvas
       style={{
-        height: "100vh",
-        width: "100vw",
+        height: "50vh",
+        width: "50vw",
         display: "flex",
         justifyContent: "center",
-        alignItems: "center",
+        alignItems: "start",
       }}
     >
       <OrbitControls enableZoom enablePan enableRotate />
       <directionalLight position={[1, 1, 1]} intensity={10} color={0x9cdba6} />
-      <color attach="background" args={["#FFFFFF"]} />
+      <color attach="background" args={["#F0F0F0"]} />
       <RotatingCube />
     </Canvas>
   );
