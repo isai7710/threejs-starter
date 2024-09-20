10d58ede145b1e5c6cd856bdde006c580e2c5cdb
isai7710 <isai10sanchez@gmail.com>
2024-09-20 13:08:27 -0600

    starting hero section

---
+++ proj/src/sections/Hero.jsx
@@ -0,0 +1,13 @@
+const Hero = () => {
+  return (
+    <section className="min-h-screen w-full flex flex-col relative">
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+        <p className="text-neutral-400">
+          Hi I&apos;m Travis $cott <span className="waving-hand">10</span>
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
