efda2f80e09e8fbd7f6ba8ce971da9dddbeee623
isai7710 <isai10sanchez@gmail.com>
2024-09-30 15:03:54 -0600

    added .jsx file for robot to render as react component within canvas component in App and added buttons with state variables to toggle wireframe and visibility

--- custom_r3f_component/src/App.jsx
+++ custom_r3f_component/src/App.jsx
@@ -1,19 +1,51 @@
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import Model from "./components/model";
+import { useState } from "react";
+import RobotModel from "./components/robot";
 
 const App = () => {
+  const [showWireframe, toggleShowWireframe] = useState(true);
+  const [showRobot, toggleShowRobot] = useState(true);
+
   return (
-    <div className="w-[50vw] h-[50vh] rounded-lg border border-green-500">
-      <Canvas className="rounded-md" gl={{ alpha: true }}>
-        <OrbitControls enableZoom enablePan enableRotate />
-        <directionalLight
-          position={[1, 1, 1]}
-          intensity={10}
-          color={0x9cdba6}
-        />
-        <Model />
-      </Canvas>
+    <div className="flex flex-col gap-2 justify-center">
+      <div className="w-[50vw] h-[70vh] rounded-lg border-2 border-green-800">
+        <Canvas
+          className="rounded-md bg-green-800 bg-opacity-10"
+          gl={{ alpha: true }}
+        >
+          <PerspectiveCamera makeDefault position={[0, 10, 40]} />
+          <OrbitControls enableZoom enablePan enableRotate />
+          <directionalLight
+            position={[1, 1, 1]}
+            intensity={10}
+            color={0x9cdba6}
+          />
+          <directionalLight
+            position={[-1, -1, -1]}
+            intensity={10}
+            color={0x9cdba6}
+          />
+          {showRobot ? (
+            <RobotModel
+              position={[0.0, -11.0, 0.0]}
+              scale={[0.11, 0.11, 0.11]}
+              showWireframe={showWireframe}
+            />
+          ) : (
+            <Model />
+          )}
+        </Canvas>
+      </div>
+      <div className="flex gap-2 justify-center">
+        <button onClick={() => toggleShowWireframe((prev) => !prev)}>
+          Toggle wireframe
+        </button>
+        <button onClick={() => toggleShowRobot((prev) => !prev)}>
+          Show {showRobot ? `Model` : `Robot`}
+        </button>
+      </div>
     </div>
   );
 };
--- custom_r3f_component/src/components/model.jsx
+++ custom_r3f_component/src/components/model.jsx
@@ -1,6 +1,6 @@
 import { useFrame } from "@react-three/fiber";
 import { useRef, useMemo } from "react";
-import { CylinderGeometry, LineBasicMaterial, EdgesGeometry } from "three";
+import * as THREE from "three";
 
 const Model = () => {
   const groupRef = useRef();
@@ -13,9 +13,9 @@ const Model = () => {
   });
 
   const [cylinderEdges, lineMaterial] = useMemo(() => {
-    const geometry = new CylinderGeometry(1, 1, 2);
-    const edgesGeometry = new EdgesGeometry(geometry, 1);
-    const material = new LineBasicMaterial({ color: 0xb85e86 });
+    const geometry = new THREE.CylinderGeometry(20, 20, 25);
+    const edgesGeometry = new THREE.EdgesGeometry(geometry, 1);
+    const material = new THREE.LineBasicMaterial({ color: 0xb85e86 });
     return [edgesGeometry, material];
   }, []);
 
